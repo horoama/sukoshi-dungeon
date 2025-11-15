2fe3f60d71123efcfbea744875cb6e886e16c2b6
horoama <horoama929@gmail.com>
2025-11-15 19:35:41 +0900

    Add hp num

--- scripts/GUI/side_ui.gd
+++ scripts/GUI/side_ui.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var level_num : Label
 @export var hp_bar : ColorRect
+@export var hp_label : Label
 
 var hp_bar_max_width : int
 
@@ -11,7 +12,8 @@ func _ready() -> void:
 func initialize(player : Entity) -> void:
     var fighter_component: FighterComponent = player.fighter_component
     if fighter_component:
-        fighter_component.hp_changed.connect( _on_hp_changed)
+        fighter_component.hp_changed.connect(_on_hp_changed)
+        _on_hp_changed(fighter_component.hp, fighter_component.max_hp)
 
 func _on_dungeon_level_changed(new_level: int) -> void:
     # format "Basement: %d"
@@ -20,3 +22,4 @@ func _on_dungeon_level_changed(new_level: int) -> void:
 func _on_hp_changed(hp: int, max_hp: int) -> void:
     var health_ratio: float = float(hp) / float(max_hp)
     hp_bar.size.x = int(health_ratio * float(hp_bar_max_width))
+    hp_label.text = "%d / %d" % [hp, max_hp]
